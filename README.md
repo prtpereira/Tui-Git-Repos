73606e77a6ab9a6585c43a9a112d62bcb3015ec9
André Pereira <andre.pereira@tomiaglobal.com>
2024-02-27 01:32:31 +0000

    Add documentation

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Tui-Git-Repos
+# TUI Service - Git Repository Manager API
+
+
+## Getting started
+
+The app consists of being able to list all github repositories of a given username, wich are not forks. Information about the branch name and the last commit sha is also present.
+
+## Start the application
+
+### Running the app
+
+```
+ docker-compose build
+ docker-compose up app
+```
+
+The above commands, will build and run a docker container, starting this web service on localhost at port 8081.
+
+### Supported endpoints
+
+- /api/users/<username>/repos
+
+### Pagination support
+You can also introduce pagination options into the endpoint.\
+Follow the below example:
+- /api/github/users/<username>/repos?page=<page_number>&size=<page_size>
+
+## Calling the API
+
+After the docker container is running, you can use an API requester of you choose, like Postman, or even using the `curl` command, to start to perform some requests into the web service.
+
+Examples:
+```
+GET http://localhost:8081/api/users/prtpereira/repos
+GET http://localhost:8081/api/users/prtpereira/repos?page=1&size=5
+```
+
+## Swagger documentation
+
+https://app.swaggerhub.com/apis/PRTPEREIRA2/tui-git-repos/1.0.0#
--- pipeline.groovy
+++ pipeline.groovy
@@ -1,30 +1,21 @@
-Resources:
- MyCluster:
-  Type: AWS::ECS::Cluster
+pipeline {
+ agent any
 
- MyTaskDefinition:
-  Type: AWS::ECS::TaskDefinition
-  Properties:
-   Family: my-task
-   Cpu: 256
-   Memory: 512
-   NetworkMode: awsvpc
-   ContainerDefinitions:
-    - Name: my-container
-      Image: your-docker-image-url
-      PortMappings:
-       - ContainerPort: 8080
-
- MyService:
-  Type: AWS::ECS::Service
-  Properties:
-   Cluster: !Ref MyCluster
-   DesiredCount: 1
-   TaskDefinition: !Ref MyTaskDefinition
-   LaunchType: FARGATE
-   NetworkConfiguration:
-    AwsvpcConfiguration:
-     Subnets:
-      - subnet-id
-     SecurityGroups:
-      - security-group-id
+ stages {
+  stage('Build') {
+   steps {
+    // Build your application here
+    sh 'mvn clean package'
+   }
+  }
+  stage('Deploy to AWS') {
+   steps {
+    // Use AWS CLI to deploy CloudFormation templates
+    withAWS(credentials: 'aws-credentials // ID of jenkins credentials', region: 'us-east-1') {
+     sh 'aws cloudformation deploy --template-file cloud-fargate-service.yaml --stack-name my-fargate-service'
+     sh 'aws cloudformation deploy --template-file cloud-api-gateway.yaml --stack-name my-api-gateway'
+    }
+   }
+  }
+ }
+}
